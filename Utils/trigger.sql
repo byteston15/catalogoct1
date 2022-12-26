766a6aa70258b290da898428b15e1285f1f6e51c
byteston <rojas.devep@gmail.com>
2022-12-26 15:53:08 -0400

    sysdate agregado

--- Utils/trigger.sql
+++ Utils/trigger.sql
@@ -4,10 +4,10 @@
 CREATE TRIGGER HISTORY_CREATE_PRICE
 AFTER INSERT ON lista_producto FOR EACH ROW 
 INSERT INTO historial (desde, hasta, monto, liquidacion, fk_lista, fk_producto, fecha) 
-VALUES ( NEW.desde, NEW.hasta, NEW.monto, NEW.liquidacion, NEW.fk_lp_listaprecio, NEW.fk_lp_producto, curdate())
+VALUES ( NEW.desde, NEW.hasta, NEW.monto, NEW.liquidacion, NEW.fk_lp_listaprecio, NEW.fk_lp_producto, sysdate())
 
 
 CREATE TRIGGER history_prices_update
 AFTER UPDATE ON lista_producto FOR EACH ROW
 INSERT INTO historial (desde, hasta, monto, liquidacion, fk_lista, fk_producto, fecha )
-VALUES (OLD.desde, OLD.hasta, OLD.monto, NEW.liquidacion, NEW.fk_lp_listaprecio, NEW.fk_lp_producto, curdate())
+VALUES (OLD.desde, OLD.hasta, OLD.monto, NEW.liquidacion, NEW.fk_lp_listaprecio, NEW.fk_lp_producto, sysdate())
